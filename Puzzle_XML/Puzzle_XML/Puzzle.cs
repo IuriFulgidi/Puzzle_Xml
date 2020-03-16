039b0fec22e8f8eadeb97432cc9a935581b51483
iurifulgidi <iurifulgidi@hotmail.com>
2020-03-16 18:40:30 +0100

    creazione classe puzzle

---
+++ Puzzle_XML/Puzzle_XML/Puzzle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzle_XML
+{
+    public class Puzzle
+    {
+        public string Name { get; set; }
+        public string NStates { get; set; }
+        public bool Solved { get; set; }
+        public int NFaces { get; set; }
+        public override string ToString()
+        {
+            if (this.Solved)
+                return $"a {Name} that's solved";
+            else
+                return $"a {Name} that's not solved";
+        }
+    }
+}
